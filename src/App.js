25ec351f0cf3b100cd87e6f885575369b7e14d19
Anusha Lihala <anusha.lihala@koneksys.com>
2025-01-10 00:59:03 +0530

    Update repo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
-import React from 'react';
-import FileUpload from './components/FileUpload';
-import './App.css';
+import React, { useState } from "react";
+import FileUpload from "./components/FileUpload";
+import Markdown from "react-markdown";
+import "./App.css";
 
-const App = () => (
-  <div className='container mt-4'>
-    <h4 className='display-4 text-center mb-4'>
-      <i className='fab fa-react' /> React File Upload
-    </h4>
+const App = () => {
+  const [llmResponse, setLlmResponse] = useState(null);
+  const [showSpinner, setShowSpinner] = useState(false);
 
-    <FileUpload />
-  </div>
-);
-
+  return (
+    <div className="container mt-4 m-4">
+      <h4 className="display-4 text-center mb-4">
+        <img src="https://cdn.prod.website-files.com/6645c0129428882861d078b8/66603a39bd44aeb85269ceea_655df7e9805dd2bd768367ef_llamaindex-removebg-preview.png" />{" "}
+        SkillSync - Tailor your CV or resume!
+      </h4>
+      {llmResponse == null && !showSpinner ? (
+        <FileUpload
+          setLlmResponse={setLlmResponse}
+          setShowSpinner={setShowSpinner}
+        />
+      ) : null}
+      {showSpinner ? (
+        <div
+          className="d-flex justify-content-center mt-6"
+          style={{ marginTop: "2.25rem" }}
+        >
+          <div
+            className="spinner-border"
+            style={{ height: "100px", width: "100px" }}
+            role="status"
+          >
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+      ) : null}
+      {llmResponse != null ? (
+        <Markdown className="mt-6 m-4">{llmResponse}</Markdown>
+      ) : null}
+    </div>
+  );
+};
 
 export default App;
--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -1,77 +1,138 @@
-import React, { Fragment, useState } from 'react';
-import Message from './Message';
-import Progress from './Progress';
-import axios from 'axios';
+import React, { Fragment, useState } from "react";
+import Message from "./Message";
+import Progress from "./Progress";
+import axios from "axios";
 
-const FileUpload = () => {
-  const [file, setFile] = useState('');
-  const [filename, setFilename] = useState('Choose File');
-  const [uploadedFile, setUploadedFile] = useState({});
-  const [message, setMessage] = useState('');
+const taskTypes = {
+  guided_cv: "Guided CV",
+  guided_cover_letter: "Guided Cover Letter",
+  auto_cv: "Autonomous CV",
+  auto_cover_letter: "Autonomous Cover Letter",
+};
+
+const FileUpload = ({ setLlmResponse, setShowSpinner }) => {
+  const [jobDesc, setJobDesc] = useState("");
+  const [taskType, setTaskType] = useState("");
+  const [file, setFile] = useState(null);
+  const [filename, setFilename] = useState("Upload CV");
+  const [message, setMessage] = useState("");
   const [uploadPercentage, setUploadPercentage] = useState(0);
 
-  const onChange = e => {
+  const onChange = (e) => {
     setFile(e.target.files[0]);
     setFilename(e.target.files[0].name);
   };
 
-  const onSubmit = async e => {
+  const onSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData();
-    formData.append('file', file);
+    if (file == null) {
+      setMessage("Please upload CV");
+      return;
+    } else if (jobDesc.length < 1) {
+      setMessage("Please provide job description");
+      return;
+    } else if (taskType.length < 1) {
+      setMessage("Please choose output type.");
+      return;
+    }
+    formData.append("file", file);
+    formData.append("jobDesc", jobDesc);
+    formData.append("taskType", taskType);
 
     try {
-      const res = await axios.post('http://127.0.0.1:5000/submit', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        },
-        onUploadProgress: progressEvent => {
-          setUploadPercentage(parseInt(Math.round((progressEvent.loaded * 100) / progressEvent.total)));
-          setTimeout(() => setUploadPercentage(0), 10000);
+      const res = await axios.post(
+        "https://anushalihala.pythonanywhere.com/submit",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+          onUploadProgress: (progressEvent) => {
+            const percent = parseInt(
+              Math.round((progressEvent.loaded * 100) / progressEvent.total)
+            );
+            if (percent == 100) {
+              setMessage("File uploaded");
+              setShowSpinner(true);
+            }
+            setUploadPercentage(percent);
+            setTimeout(() => setUploadPercentage(0), 10000);
+          },
         }
-      });
+      );
 
-      const { fileName, filePath } = res.data;
-      setUploadedFile({ fileName, filePath });
-      setMessage('File uploaded');
-    } catch(err) {
-      if(err.response.status === 500) {
-        setMessage('There was a problem witht he server');
+      setShowSpinner(false);
+      setLlmResponse(res.data);
+    } catch (err) {
+      if (err.response.status === 500) {
+        setMessage("There was a problem with he server");
       } else {
-        setMessage(err.response.data.msg);
+        console.log(err);
+        setMessage("Error uploading file");
       }
     }
-  }
+  };
 
   return (
     <Fragment>
-      { message ? <Message msg={ message } /> : null }
+      {message ? <Message msg={message} /> : null}
       <form onSubmit={onSubmit}>
+        <div className="mb-4" style={{ display: "flex" }}>
+          <label className="mr-2" htmlFor="jobDesc">
+            Job Description
+          </label>
+          <textarea
+            id="jobDesc"
+            style={{ width: "100%", height: "200px" }}
+            onChange={(e) => setJobDesc(e.target.value)}
+          />
+        </div>
         <div className="custom-file mb-4">
           <input
             type="file"
             className="custom-file-input"
             id="customFile"
+            style={{ height: "0px !important" }}
             onChange={onChange}
           />
-          <label className='custom-file-label' htmlFor='customFile'>
+          <label className="custom-file-label" htmlFor="customFile">
             {filename}
           </label>
         </div>
-
-        <Progress percentage={ uploadPercentage } />
+        <div className="custom-file mb-2">
+          <label className="mr-2" htmlFor="taskType">
+            Choose Output Type
+          </label>
+          <select id="taskType" onChange={(e) => setTaskType(e.target.value)}>
+            <option value={""}></option>
+            {Object.entries(taskTypes).map((task) => (
+              <option value={task[0]} key={task[0]}>
+                {task[1]}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <p>
+            Guided means there are lower level step by step instructions
+            provided by a human, for example 'what are the hard skills required
+            by the job?', 'where does the user demonstrate experience with these
+            hard skills?'. This method might be more reliable but slower.
+          </p>
+          <p>
+            Autonomous means that the AI handles data fetching and synthesis
+            independently after being given a single prompt.
+          </p>
+        </div>
+        <Progress percentage={uploadPercentage} />
 
         <input
           type="submit"
-          value="Upload"
-          className="btn btn-primary btn-block mt-4"
+          value="Submit"
+          className="btn btn-primary btn-block mt-4 mb-6"
         />
       </form>
-      { uploadedFile ? <div className="row mt-5">
-        <div className="col-md-6 m-auto"></div>
-          <h3 classNAme="text-center">{ uploadedFile.fileName }</h3>
-          <img style={{ width: '100%' }} src={uploadedFile.filePath} alt="" />
-        </div> : null }
     </Fragment>
   );
 };
