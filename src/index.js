3ac6b5fcec22f5f5d6b51a0ff35bb8bebc7493f9
Anusha Lihala <anusha.lihala@koneksys.com>
2025-01-09 17:17:29 +0530

    Init repo

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { render } from "react-dom";
+import {createRoot} from 'react-dom/client';
+import App from './App';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+root.render(<App/>)
+// ReactDOM.render(<App />, document.getElementById('root'));
